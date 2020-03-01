e52b8e330f22140c26b7b7eda2551695ae3eb6d4
Alex Tse(mac) <alexandtse@hotmail.com>
2020-03-01 09:17:40 +0000

    Initial commit 1. setup application structure by using spring initializer 2. implements commandLineRunner for console application

---
+++ src/test/java/uk/co/sainsburys/HtmlScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.sainsburys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
